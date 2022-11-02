c3aed3e12cbfad842fbf2161b5296693742071e7
kaivon tolooee <kaivont@gmail.com>
2022-11-02 15:25:02 -0600

    initial upload

---
+++ gallery.js
@@ -0,0 +1,48 @@
+const galleryList = [{
+      image: "./images/rowley2.jpg",
+      title: "10970 Rowley Drive",
+      type: "Brokerage Residential",
+      website: "https://10970rowleydrive.com/"
+    },
+    {
+      image: "./images/Whileway.jpg",
+      title: "3112 W Whileway Circle",
+      type: "Brokerage Residential",
+      website: "https://3112wwhileawaycircle.com/"
+    },
+    {
+      image: "./images/BarrelRace.jpg",
+      title: "6578 Barrel Race Drive",
+      type: "Brokerage Residential",
+      website: "https://6578barrelracedrive.com/"
+    },
+    {
+      image: "./images/SingleLeaf.jpg",
+      title: "13855 Single Leaf Court",
+      type: "Brokerage Residential",
+      website: "https://13855singleleafct.com/"
+    },
+    {
+      image: "./images/Prospect.jpeg",
+      title: "424 N Prospect Street",
+      type: "Brokerage Remodels",
+      website: "https://424nprospectstreet.com/"
+    },
+    {
+      image: "./images/Heathland.jpeg",
+      title: "5738 Heathland Terrace",
+      type: "Brokerage Remodels",
+      website: "https://5738heathlandterrace.com/"
+    },
+    {
+      image: "./images/Otowi.jpeg",
+      title: "39 Otowi Drive",
+      type: "Brokerage Remodels",
+      website: "https://39otowidrive.com/"
+    },
+    {
+      image: "./images/Ellston.jpg",
+      title: "3823 Ellston Place",
+      type: "Brokerage Remodels",
+      website: "https://3823ellstonplace.com/"
+    }]
